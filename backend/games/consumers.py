daded180712f2575205640d263f2f1035852bec1
BAndris4 <andrisbalogh@gmail.com>
2025-12-27 18:49:03 +0100

    feat(multiplayer): implement game models and websocket foundation

---
+++ backend/games/consumers.py
@@ -0,0 +1,31 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.game_id = self.scope['url_route']['kwargs']['game_id']
+        self.room_group_name = f'game_{self.game_id}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        pass
+
+    async def game_message(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
